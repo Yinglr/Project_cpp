2cff7d947f218d57e9e69ecbf72045a6a0b2fdbf
Ying_lr <ying_lr@hotmail.com>
2017-12-31 13:33:48 +0100

    hedged portfolio

--- functions.cpp
+++ functions.cpp
@@ -8,10 +8,198 @@ namespace project
 	{
 		
 		
+		double maturity(struct std::tm& start, struct std::tm& end)
+		{
+			double difftime = std::difftime(start, end);
+			return TS::difftime_to_years(difftime); // base ACT/365 for simplicity (see TS::difftime_to_years)
+		}
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		/* -------------------------------- */
 		/* ---- DELTA-HEDGED PORTFOLIO ---- */
 		/* -------------------------------- */
+		
+		// constructors
+		hedged_ptf::hedged_ptf(const std::string& name, std::ifstream& csv_file,
+							   double strike, double rate, double div)
+			: m_ts(name, csv_file), m_strike(strike), m_rate(rate), m_div(div)
+		{
+			m_start = 1;
+			m_end = m_ts.get_size();
+		}
+		
+		
+		
+		// destructor
+		hedged_ptf::~hedged_ptf()
+		{
+			std::cout << "Deletion of hedged_ptf object " << get_name() << std::endl;
+		}
+		
+		
+		
+		
+		
+		
+		// access - general
+		std::string hedged_ptf::get_name() const
+		{
+			return m_ts.get_name();
+		}
+		
+		std::size_t hedged_ptf::get_size() const
+		{
+			return m_ts.get_size();
+		}
+		
+		std::size_t hedged_ptf::get_size_range() const
+		{
+			return m_end - m_start + 1;
+		}
+		
+		
+		// access - values
+		double hedged_ptf::get_spot() const
+		{
+			return m_ts[m_start];
+		}
+		
+		double hedged_ptf::get_maturity() const
+		{
+			return maturity(m_ts.get_date(m_end), m_ts.get_date(m_start));
+		}
+		
+		double hedged_ptf::get_strike() const
+		{
+			return m_strike;
+		}
+		
+		double hedged_ptf::get_rate() const
+		{
+			return m_rate;
+		}
+		
+		double hedged_ptf::get_div() const
+		{
+			return m_div;
+		}
+		
+		
+		
+		
+		
+		
+		// access - time_series
+		const TS::time_series& hedged_ptf::get_ts() const
+		{
+			return m_ts;
+		}
+		
+		
+		
+		
+		// access - date range
+		std::size_t hedged_ptf::get_start() const
+		{
+			return m_start;
+		}
+		
+		std::size_t hedged_ptf::get_end() const
+		{
+			return m_end;
+		}
+		
+		
+		
+		
+		// printing
+		void hedged_ptf::print_info() const
+		{
+			std::cout << std::endl;
+			std::cout << "---------------------------------" << std::endl;
+			std::cout << "General info on hedged_ptf object " << get_name() << std::endl;
+			std::cout << "---------------------------------" << std::endl;
+			std::cout << "Nb of elements (range):           " << get_size() << std::endl;
+			std::cout << "Nb of elements (interior range):  " << get_size_range() << std::endl;
+			std::cout << "Start of total range:             " << TS::to_string(m_ts.get_date(1)) << std::endl;
+			std::cout << "Start of interior range:          " << TS::to_string(m_ts.get_date(m_start)) << std::endl;
+			std::cout << "End of interior range:            " << TS::to_string(m_ts.get_date(m_end)) << std::endl;
+			std::cout << "End of total range:               " << TS::to_string(m_ts.get_date(get_size())) << std::endl;
+			std::cout << "---------------------------------" << std::endl;
+			std::cout << std::endl;
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		// modify - general
+		void hedged_ptf::let_name(std::string name)
+		{
+			m_ts.let_name(name);
+		}
+		
+		
+		// modify - values
+		void hedged_ptf::let_strike(double strike)
+		{
+			if(strike<=0)
+			{
+				std::cout << "Error: negative strike on portfolio " << get_name() << std::endl;
+			}
+			else
+			{
+				std::cout << "Strike of portfolio " << get_name() << " set to " << strike << std::endl;
+				m_strike = strike;
+			}	
+		}
+		
+		void hedged_ptf::let_rate(double rate)
+		{
+			// no constraint as rates can actually go negative!
+			std::cout << "Rate of portfolio " << get_name() << " set to " << rate << std::endl;
+			m_rate = rate;
+		}
+		
+		void hedged_ptf::let_div(double div)
+		{
+			if(div<=0)
+			{
+				std::cout << "Error: negative dividends on portfolio " << get_name() << std::endl;
+			}
+			else
+			{
+				std::cout << "Dividends of portfolio " << get_name() << " set to " << div << std::endl;
+				m_div = div;
+			}
+		}
+		
+		
+		// modify - date range
+		void hedged_ptf::let_start(std::size_t start)
+		{
+			std::cout << "Start of portfolio " << get_name() << " set to " << start
+					<< " (" << TS::to_string(m_ts.get_date(start)) << ")" << std::endl;
+			m_start = start;
+		}
+		
+		void hedged_ptf::let_end(std::size_t end)
+		{
+			std::cout << "End of portfolio " << get_name() << " set to " << end
+					<< " (" << TS::to_string(m_ts.get_date(end)) << ")" << std::endl;
+			m_end = end;
+		}
+		
 		
 		
 		
--- functions.hpp
+++ functions.hpp
@@ -22,9 +22,109 @@ namespace project
 	namespace BS
 	{
 
+		
+		double maturity(struct std::tm& start, struct std::tm& end);
+		
+		
+		
+		
 		/* -------------------------------- */
 		/* ---- DELTA-HEDGED PORTFOLIO ---- */
 		/* -------------------------------- */
+		
+		// class of the delta-hedged portfolio we will manipulate
+		class hedged_ptf
+		{
+		public:
+			
+			// constructors
+			hedged_ptf(const std::string& name, std::ifstream& csv_file,
+					   double strike = 100, double rate = 0, double div = 0);
+			
+			// destructor
+			~hedged_ptf();
+			
+			// access - general
+			std::string get_name() const;
+			std::size_t get_size() const;
+			std::size_t get_size_range() const; // between start and end
+
+			
+			// access - values
+			double get_spot() const;
+			double get_maturity() const;
+			double get_strike() const;
+			double get_rate() const;
+			double get_div() const;
+			
+			// access - time_series
+			const TS::time_series& get_ts() const;
+			
+			// access - date range
+			std::size_t get_start() const;
+			std::size_t get_end() const;
+			
+			
+			// printing
+			void print_info() const;
+			// void print_range() const;
+			
+			
+			
+			// modify - general
+			void let_name(std::string name);
+			
+			// modify - values
+			void let_strike(double strike);
+			void let_rate(double rate);
+			void let_div(double div);
+			
+			// modify - date range
+			// !!!!!
+			// a faire : gérer le cas où ça se croise // cas out of range
+			// !!!!!
+			void let_start(std::size_t start);
+			void let_end(std::size_t end);
+			// void let_range(std::size_t start, std::size_t end);
+			
+			
+			// // sets range to last n months (for vol computations)
+			// void let_last_range(std::size_t n, bool next = true)); // uses shift_months
+			
+			
+			// // P&L computations
+			// double get_pnl(double vol) const;
+			// double get_vol() const;
+			
+			
+		private:
+			
+			// data members
+			
+			// parameters
+			double m_strike;
+			double m_rate;
+			double m_div;
+			
+			// time_series
+			TS::time_series m_ts;
+			std::size_t m_start;
+			std::size_t m_end;
+			
+			
+		};
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		
 		
